815a2c19e87316ef772f2598ba16aa8dc5c325e1
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-07 04:45:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,20 @@
 def addition(num1, num2)
-  
+  sum = num1 + num2
+  p sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  p difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  p quotient
 end
 
 def multiplication(num1, num2)
-
+  
 end
 
 def modulo(num1, num2)
